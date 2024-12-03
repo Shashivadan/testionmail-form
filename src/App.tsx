310f19ac92dac5a95918dfade69530fd29eeeff3
Shashivadan <shashivadan99@gmail.com>
2024-12-03 17:30:45 +0530

    feat: added test

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { TestimonialForm } from "./components/index";
+
+function App() {
+  return (
+    <>
+      <TestimonialForm />
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path, { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tailwindcss from "tailwindcss";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+export default defineConfig({
+  plugins: [react(), dts({ rollupTypes: true }), cssInjectedByJsPlugin()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "testimonial-form",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
